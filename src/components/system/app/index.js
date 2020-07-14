478990eb2c142a1bdc719875f84faf8c9f02e124
Gerardo Rico Botella <grico@wearemarketing.com>
2020-07-14 20:40:26 +0200

    Initial commit

---
+++ src/components/system/app/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
