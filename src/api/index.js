3a2cc949d6f3e9b8b288954de0e401c64c4e91c4
Gerardo Rico Botella <grico@wearemarketing.com>
2020-07-14 21:05:19 +0200

    Add character related api functions

---
+++ src/api/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import {BASE_URL} from '../config/api';
+import qs from 'qs';
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  }
+});
+
+export const getCharacters = (params) => {
+  const url = `/characters?${qs.stringify(params, {skipNulls: true})}`;
+  return instance.get(url);
+}
+
+export const getCharacter = (id) => {
+  const url = `/characters/${id}`;
+  return instence.get(url);
+}
